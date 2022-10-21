72a4d9c6404872f3a191510410841328d2295920
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2022-10-21 03:46:04 -0300

    Create tipos.php

---
+++ Aula 04/tipos.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <style>
+        body{
+            background-color: black;
+            color:white;
+        }
+        div{
+            font-size: 30px;
+        }
+
+    </style>
+</head>
+<body>
+
+<div>
+
+
+
+    <?php
+    $idade =22;
+    $nome = "Caio";
+    /*Duas formas diferentes de utilizar a concatenação de frases com variáveis */
+   /* echo  $nome . " tem " . $idade . " anos " ;  */
+
+   /* echo "$nome tem $idade anos" ; */
+
+
+    ?>
+
+</div>
+
+
+</body>
+</html>
